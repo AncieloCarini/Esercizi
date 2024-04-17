9a6f1366017eb61cfe53168c6585deffaee21f53
AncieloCarini <angelolocarini04@gmail.com>
2024-04-17 13:49:45 +0200

    File lezione2.py aggiunto

---
+++ Lezione2/lezione2.py
@@ -0,0 +1,17 @@
+# angelo locarini
+# 17/04/2024
+
+#print("hello world!")
+
+#2-3. Personal Message: 
+#Use a variable to represent a person’s name, 
+#and print a message to that person. 
+#Your message should be simple,such as, 
+#“Hello Eric, would you like to learn some Python today?”
+
+
+
+name: str = "Giovanni"
+massage: str = f"{name}, ti va di imparare un po di python insieme?"
+print(massage)
+
