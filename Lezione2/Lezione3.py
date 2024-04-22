b68528cb610f68a6f18f78e3888f3840be3cf1a4
AncieloCarini <angelolocarini04@gmail.com>
2024-04-22 13:16:09 +0200

    aggiornato

--- Lezione2/Lezione3.py
+++ Lezione2/Lezione3.py
@@ -1,3 +1,4 @@
+'''
 4-1. Pizzas: Think of at least three kinds of your favorite pizza. 
 Store these pizza names in a list, and then use a for loop to print the name of each pizza.
 • Modify your for loop to print a sentence using the name of the pizza, 
@@ -6,4 +7,21 @@ you should have one line of output containing a simple statement like I like pep
 • Add a line at the end of your program, outside the for loop, that states how much you like pizza. 
 The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence,
 such as I really love pizza!
+
+
+favpizze: list = ("margherita", "diavola", "boscaiola")
+print(favpizze)
+
+for i in favpizze:
+    print(f"my favourite pizza is {i}")
+
+
+    
+    4-2. Animals: Think of at least three different animals that have a common characteristic. 
+    Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+• Modify your program to print a statement about each animal, such as A dog would make a great pet.
+• Add a line at the end of your program, stating what these animals have in common. You could print a sentence, 
+such as Any of these animals would make a great pet!
 '''
+
+
