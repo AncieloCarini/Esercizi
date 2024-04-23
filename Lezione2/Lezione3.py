15a1d367464a0176c3cf8ac6216305e27e5c5035
AncieloCarini <angelolocarini04@gmail.com>
2024-04-23 13:45:03 +0200

    aggiornato

--- Lezione2/Lezione3.py
+++ Lezione2/Lezione3.py
@@ -22,6 +22,328 @@ for i in favpizze:
 • Modify your program to print a statement about each animal, such as A dog would make a great pet.
 • Add a line at the end of your program, stating what these animals have in common. You could print a sentence, 
 such as Any of these animals would make a great pet!
+
+
+animals: list = ("cane", "gatto", "cinghiale")
+print(animals)
+for i in animals:
+    print(f"il {i} è un animale molto peloso e carino")
+    
+
+
+4-3. Counting to Twenty: Use a for loop to print the numbers from 1 to 20, inclusive.
+
+numeri: list = [x for x in range(1, 21)]
+print(numeri)
+
+for x in numeri:
+    print(x)
+'''
+''' 
+
+4-4. One Million: Make a list of the numbers from one to one million, and then use a for loop to print the numbers.
+(If the output is taking too long, stop it by pressing CTRL-C or by closing the output window.)
+
+
+numeri: list = [x for x in range(1, 1000001)]
+print(numeri)
+
+for x in numeri:
+    print(x)
+
+
+
+4-5. Summing a Million: Make a list of the numbers from one to one million,
+and then use min() and max() to make sure your list actually starts at one and ends at one million.
+Also, use the sum() function to see how quickly Python can add a million numbers.
+
+
+numeri: list = [x for x in range(1, 1000001)]
+print(numeri)
+print(min(numeri))
+print(max(numeri))
+
+
+4-6. Odd Numbers: Use the third argument of the range() 
+function to make a list of the odd numbers from 1 to 20.
+Use a for loop to print each number.
+    
+
+numeri: list = [x for x in range(1, 21, 4)]
+print(numeri)
+
+
+4-7. Threes: Make a list of the multiples of 3, from 3 to 30. Use a for loop to print the numbers in your list.
+
+numeri: list = [x for x in range(3, 31, 3)]
+print(numeri)
+
+
+4-8. Cubes: A number raised to the third power is called a cube.
+For example, the cube of 2 is written as 2**3 in Python. Make a list of the first 10 cubes 
+(that is, the cube of each integer from 1 through 10), 
+and use a for loop to print out the value of each cube.
+
+
+numeri: list = [x**1 for x in range(1, 11)]
+print(numeri)
+
+
+4-9. Cube Comprehension: Use a list comprehension to generate a list of the first 10 cubes.
+
+
+numeri: list = [x**3 for x in range(1, 11)]
+print(numeri)
+
+
+4-10. Slices: Using one of the programs you wrote in this chapter, add several lines to the end of the program that do the following:
+• Print the message The first three items in the list are:. Then use a slice to print the first three items from that program’s list.
+• Print the message Three items from the middle of the list are:. Then use a slice to print three items from the middle of the list.
+• Print the message The last three items in the list are:. Then use a slice to print the last three items in the list.
+
+
+numeri: list = [x**1 for x in range(1, 11)]
+print(numeri)
+
+print(numeri[0:3])
+print(f"the first three items in the list are {numeri[0:3]}")
+print(f"three items in the list are {numeri[4:7]}")
+print(f"the last three items in the list are {numeri[7:10]}")
+
+
+4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. Make a copy of the list of pizzas,
+ and call it friend_pizzas. Then, do the following:
+• Add a new pizza to the original list.
+• Add a different pizza to the list friend_pizzas.
+• Prove that you have two separate lists. Print the message My favorite pizzas are:
+and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, 
+and then use a for loop to print the second list.
+Make sure each new pizza is stored in the appropriate list.
+
+
+favpizze: list = ("margherita", "diavola", "boscaiola", "marinara")
+print(favpizze)
+
+friend_pizzas: list = ("margherita", "diavola", "boscaiola", "napoli")
+print(friend_pizzas)
+
+for i in favpizze:
+    print(f"my favourite pizza is {i}")
+
+for i in friend_pizzas:
+    print(f"My friend’s favorite pizzas are {i}")
+
+
+    4-12. More Loops: All versions of foods.py in this section have avoided using for loops when printing, to save space.
+      Choose a version of foods.py, and write two for loops to print each list of foods.
+
+
+favpizze: list = ("margherita", "diavola", "boscaiola", "marinara")
+print(favpizze)
+
+friend_pizzas: list = ("margherita", "diavola", "boscaiola", "napoli")
+print(friend_pizzas)
+
+for i in favpizze and friend_pizzas:
+    print(favpizze, friend_pizzas)
+    
+
+
+5-1. Conditional Tests: Write a series of conditional tests. Print a statement
+describing each test and your prediction for the results of each test. Your code
+should look something like this:
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+• Look closely at your results, and make sure you understand why each line
+evaluates to True or False.
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another
+5 tests evaluate to False.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+print("Is car > 'lamborghini'? I predict False.")
+print(car > 'laborghini' )
+print("Is car > 'fiat'? I predict True.")
+print(car > 'fiat' )
+
+
+
+
+5-2. More Conditional Tests: You don’t have to limit the number of tests you cre-
+ate to 10. If you want to try more comparisons, write more tests and add them
+
+to conditional_tests.py. Have at least one True and one False result for each of
+the following:
+• Tests for equality and inequality with strings
+• Tests using the lower() method
+• Numerical tests involving equality and inequality, greater than and less
+than, greater than or equal to, and less than or equal to
+• Tests using the and keyword and the or keyword
+• Test whether an item is in a list
+• Test whether an item is not in a list
+
+
+numero = '20'
+print("Is numero > '12'? I predict True.")
+print(numero > '12')
+print("Is numero <= '11'? I predict False.")
+print(numero < '11')
+
+
+
+5-3. Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
+• Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
+• Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)
+
+
+alien_color = "green"
+print("is alien_color = green? I predict True")
+print(alien_color == "green")
+
+
+
+5-4. Alien Colors #2: Choose a color for an alien as you did in Exercise 5-3, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.
+
+
+
+alien_color = "green"
+print("is alien_color = green? I predict True")
+if alien_color == "green":
+    print("Player just earner 5 points")
+else:
+    print("Player just earner 10 points")
+
+
+5-5. Alien Colors #3: Turn your if-else chain from Exercise 5-4 into an if-elif-else chain.
+
+• If the alien is green, print a message that the player earned 5 points.
+• If the alien is yellow, print a message that the player earned 10 points.
+• If the alien is red, print a message that the player earned 15 points.
+• Write three versions of this program, making sure each message is printed for the appropriate color alien.
+
+
+alien_color = "green"
+print("is alien_color = green? I predict True")
+if alien_color == "green":
+    print("Player just earner 5 points")
+elif alien_color == "yellow":
+        print("Player just earner 10 points")
+elif alien_color == "red":
+        print("Player just earner 10 points")
+
+
+
+5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+• If the person is age 65 or older, print a message that the person is an elder.
+
+
+age = "1"
+print("is age < '2'? I predict True")
+if age < "2":
+    print("is a baby")
+elif age < "4":
+        print("is a toddler")
+elif age < '13':
+        print("is a kid")
+elif age < "20":
+        print("is a teenager")
+elif age < '65':
+        print("is a adult")
+elif age > '65':
+        print("is a elder")
+
+
+
+5-7. Favorite Fruit: Make a list of your favorite fruits, and then write a series of independent if statements 
+that check for certain fruits in your list.
+• Make a list of your three favorite fruits and call it favorite_fruits.
+• Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, 
+the if block should print a statement, such as You really like Apples!
+
+
+Favorite_fruit: list = ["mela", "banana", "kiwi", "pera", "melone"]
+print(Favorite_fruit)
+Favorite_fruits: list = ["banana", "melone", "mela"]
+print(Favorite_fruits)
+
+if "kiwi" in Favorite_fruit:
+        print("kiwi is in fruit")
+
+if "ki" in "kiwi":
+        print("È una sottostringa")
+if "pera" in Favorite_fruits:
+        print("pera è presente")
+
+
+
+
+5-8. Hello Admin: Make a list of five or more usernames, including the name 'admin'.
+ Imagine you are writing code that will print a greeting to each user after they log in to a website.
+   Loop through the list, and print a greeting to each user.
+• If the username is 'admin', print a special greeting, such as Hello admin, would you like to see a status report?
+• Otherwise, print a generic greeting, such as Hello Jaden, thank you for logging in again.  
+
+usernames: list = ['admin', 'angelo', 'gabriele', 'giuseppe', 'giovanni']
+print(usernames)
+
+for i in usernames:
+    if i == "admin":
+        print(f"hi {i} how are you?")
+    else:    
+        print(f"hi {i} how are you?")
+
+
+
+5-9. No Users: Add an if test to hello_admin.py to make sure the list of users is not empty.
+• If the list is empty, print the message We need to find some users!
+• Remove all of the usernames from your list, and make sure the correct message is printed.
+
+
+usernames: list = ['admin', 'angelo', 'gabriele', 'giuseppe', 'giovanni']
+print(usernames)
+
+del(usernames[0:])
+
+print(usernames)
+if usernames == []:
+    print("We need to find some users!")
+
+
+
+
+
+
+5-10. Checking Usernames: Do the following to create a program that simulates how websites ensure that everyone has a unique username.
+• Make a list of five or more usernames called current_users.
+• Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
+• Loop through the new_users list to see if each new username has already been used. If it has, 
+print a message that the person will need to enter a new username. If a username has not been used, 
+print a message saying that the username is available.
+• Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted. 
+(To do this, you’ll need to make a copy of current_users containing the lowercase versions of all existing users.)
 '''
 
+current_users: list = ['admin', 'angelo', 'gabriele', 'giuseppe', 'giovanni']
+print(current_users)
 
+new_users: list = ['admin', 'angelo', 'gianluca', 'piermario', 'giangiovanni']
+print(new_users)
+
+for x in current_users:
+    for y in new_users:
+        if y == x :
+            print(f"{y}, gia in uso")
